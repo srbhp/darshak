a1ac1614da79d6590e3e7e08a2eeb635b09b9feb
Saurabh Pradhan <pradhanphy@gmail.com>
2023-12-30 20:28:43 +0100

    Update Readme

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
@@ -14,8 +14,8 @@ setup(name='darshak',
       version='0.1.0',
       description='darshak for pdf viewer',
       long_description=readme,
-      author='Kenneth Reitz',
-      author_email='',
+      author='Saurabh Pradhan',
+      author_email='pradhanphy@gmail.com',
       url='https://github.com/srbhp/darshak',
       license=license,
       packages=find_packages(exclude=('tests', 'docs')))
